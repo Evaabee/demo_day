5ac70deeff360d515915f278d70d2a7d35e56780
Molly Zhang <yz9753@nyu.edu>
2024-11-21 03:56:57 -0500

    Update Dropdown.js

--- frontend/src/components/Dropdown.js
+++ frontend/src/components/Dropdown.js
@@ -1,15 +1,15 @@
-// src/components/Dropdown.js
 import React from 'react';
-import './Dropdown.css';
 
 const Dropdown = ({ label, options, onSelect, selectedValue }) => {
   return (
     <div>
       <label>{label}</label>
-      <select value={selectedValue} onChange={(e) => onSelect(e.target.value)}>
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
+      <select 
+        value={selectedValue} 
+        onChange={(e) => onSelect(e.target.value)}>
+        {options.map((option, index) => (
+          <option key={index} value={option.value || option}>
+            {option.label || option} 
           </option>
         ))}
       </select>
