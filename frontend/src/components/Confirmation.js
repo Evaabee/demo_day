4fae0dd770e9e714ae81418d9309be24e1b17389
Molly Zhang <yz9753@nyu.edu>
2024-11-21 04:01:05 -0500

    Update Confirmation.js

--- frontend/src/components/Confirmation.js
+++ frontend/src/components/Confirmation.js
@@ -1,14 +1,16 @@
+// frontend/src/components/Confirmation.js
+import React from 'react';
 import Button from './Button';
 
 export default function Confirmation({ alertData, onModify }) {
   return (
     <div>
       <h1>Alert Setup Complete!</h1>
-      <p>Cryptocurrency: {alertData.selectedCrypto}</p>
-      <p>Alert Type: {alertData.notificationType}</p>
-      <p>Threshold Value: {alertData.thresholdValue}</p>
-      <p>Notification Method: {alertData.notificationMethod}</p>
-      
+      <p>Cryptocurrency ID: {alertData.crypto_id}</p>
+      <p>Notification Type: {alertData.notification_type}</p>
+      <p>Threshold Value: {alertData.threshold_price}</p>
+      <p>Notification Method: {alertData.notification_method}</p>
+
       {/* Two buttons for actions */}
       <Button text="Modify Settings" onClick={onModify} />
       <Button text="Done" onClick={() => alert('You will be notified for changes!')} />
